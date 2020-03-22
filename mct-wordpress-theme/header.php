6be2ff4394013ad839dffd3b5065f28ae7a1fb04
Wojciech Czarnocki <30754368+caren1@users.noreply.github.com>
2020-03-22 21:09:11 +0100

    Few styling corrections after Artur's feedback.

---
+++ mct-wordpress-theme/header.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MC Trade - Usługi Ogólnobudowlane | Wykończenie i renowacja</title>
+    <meta name="description" content="Głównym zakresem działalności są usługi ogólnobudowlane oraz remontowe, jak i renowacja i konserwacja wnętrz. Firma zaangażowana jest również w dziedzinę fotowoltaiki oraz
+    pośrednictwa w rekrutacji pracowników.">
+    <meta name="keywords"
+        content="MC Trade, Firma, Ogólnobudowlane, Usługi, Remonty, Renowacje, Elbląg, Trójmiasto, Pomorze, Pomorskie, Mazowieckie, warmińsko-mazurskie, MCTrade Sp. z o.o., Sp. z o.o.">
+    <meta name=”robots” content="index, follow">
+    <!-- <meta property="og:image" content="/img/smlogo.svg"> -->
+    <meta name="google-site-verification" content="FQkNL8y90gkYpNwWz__RPp7SRWBL-dOXjwIO2Nw_wW8" />
+    <link rel="shortcut icon" href="/img/favicon.ico">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" media="all" />
+    <!-- <link rel="stylesheet" href="css/main.css" /> -->
+    <!-- <link rel="stylesheet" href="css/main.scss" /> -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</head>
+
+<body>
+    <div class="backdrop"></div>
+
+    <header class="main-header">
+        <nav class="main-header__navigation">
+
+            <div class="main-header__navigation-left">
+                <img src="/img/final logo.svg" alt="company logo" class="navigation-left__logo">
+            </div>
+
+            <div class="main-header__navigation-right">
+
+                <div class="navigation-right__contact">
+                    <ul class="right__contact__list">
+                        <li><a href="mailto:kontakt@mc-trade.pl">kontakt@mc-trade.pl</a></li>
+                        <li><a href="tel:+48572675106">(+48) 572-675-106</a></li>
+                        <li><a href="tel:+48572675106" class="btn-primary">Zadzwoń</a></li>
+                    </ul>
+                </div>
+                <hr />
+                <div class="navigation-right__menu">
+                    <ul class="right-menu__list">
+                        <li class="list-item"><a href="#firma">Firma</a></li>
+                        <li class="list-item">
+                            <a href="#projekty">Projekty</a></li>
+                        <li class="list-item"><a href="#oferta">Oferta</a></li>
+                        <li class="list-item"><a href="#praca">Praca</a></li>
+                        <li class="list-item"><a href="#kontakt">Kontakt</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main id="intro">
+        <div class="backtotop">
+            <a href="#intro"><i class="arrow top"></i></a>
+        </div>
+
+        <section class="intro-section">
+            <div class="intro-section__logo">
+                <img src="/img/final logo.svg" alt="company logo">
+            </div>
+            <div class="intro-section__arrow">
+                <i class="arrow left"></i>
+            </div>
+        </section>
+
+        <section class="home-section">
+            <h1>MC Trade Sp. z o.o.</h1>
+            <p>Usługi ogólnobudowlane - Jakość, precyzja oraz terminowość</p>
+            <a href="#firma"><button class="btn-primary">Więcej</button></a>
+        </section>
+
+        <section id="firma" class="about-company-section">
+            <div data-aos="fade-in" class="about-company-section__introduction">
+                <h2>O Firmie</h2>
+                <p>
+                    <strong>Firma MC Trade Sp. z o.o.</strong> istnieje na rynku już od
+                    2011 roku i ma na swoim koncie wiele udanych realizacji oraz
+                    projektów na terenie całej Polski. <br /><br />
+
+                    Głównym zakresem działalności są <strong>usługi ogólnobudowlane oraz remontowe</strong>, jak
+                    i renowacja i konserwacja wnętrz. Firma zaangażowana jest również w dziedzinę fotowoltaiki oraz
+                    pośrednictwa w rekrutacji
+                    pracowników. <br /><br />
+
+                    W celu poznania szczegółów na temat naszej firmy, zapraszamy do
+                    zapoznania się z naszą ofertą. Jeżeli są Państwo zainteresowani
+                    nawiązaniem współpracy prosimy o kontakt telefoniczny, mailowy, bądź
+                    z wykorzystaniem formularza.
+                    <br /><br />
+
+                    Szukamy za równo nowych projektów, podwykonawców jak i pracowników z
+                    branży budowlanej.
+                </p>
+            </div>
+
+            <div data-aos="fade-in" id="projekty" class="about-company-section__showcase">
+                <h2>NASZE REALIZACJE</h3>
+                    <hr />
+                    <div class="showcase__slider slide1">
+                        <i class="arrow left"></i>
+
+                        <div class="showcase__slider__content">
+
+                            <div class="showcase__slider__image">
+                                <img src="/img/measuring.jpg" alt="kościółśwmarcinka" />
+                            </div>
+
+                            <div class="showcase__slider__text">
+                                <h4>Kościół Św. Marcinków</h4>
+                                <p>21.02.2020</p>
+                                <p>
+                                    Renowacja, malowanie, konserwacja ścian oraz sutfitu, wymiana
+                                    okien oraz drzwi, przygotowanie pod wymianę podłogi.
+                                </p>
+                                <a href="#">więcej.</a>
+                            </div>
+
+                        </div>
+                        <i class="arrow right"></i>
+                    </div>
+
+            </div>
+            <a href="#" class="btn-primary">Zobacz wszystkie realizacje</a>
+
+            <div class="about-company-section__projects">
+
+            </div>
+        </section>
+
+        <div class="" id="oferta"></div>
+        <section class="offer-section">
+
+            <div data-aos="fade-in" class="offer-section__introduction">
+
+                <h1>Oferta</h1>
+                <p>
+                    Zapraszamy Państwa do zapoznania się ze szczegółami naszej oferty. Ze względu na szeroki zakres
+                    naszych
+                    usług, z pewnością dopasujemy się do wszelkich potrzeb. Wybierając naszą firmę, gwarantujemy Państwu
+                    rzetelne przygotowanie realizacji wraz z wyceną oraz doradztwem, wysokiej jakości materiały od
+                    wiodących
+                    dostawców oraz terminowe realizacje.
+
+                    Z sukcesem realizujemy projekty naszych klientów, dzięki czemu ich liczba wzrasta.
+                </p>
+            </div>
+
+
+
+            <div data-aos="fade-in" class="offer-section__cards">
+                <div class="offer-section__cards-card">
+                    <div class="card-icon">
+                        <img src="/icons/023-wrench.svg" alt="wrench" />
+                    </div>
+                    <div class="card-description">
+                        <h3>Usługi Ogólnobudowlane</h3>
+                        <a class="ogolne">więcej</a>
+                    </div>
+                </div>
+                <div class="offer-section__cards-card">
+                    <div class="card-icon">
+                        <img src="/icons/019-paint roller.svg" alt="paint roller" />
+                    </div>
+                    <div class="card-description">
+                        <h3>Wykończenia</h3>
+                        <a class="wykonczenia">więcej</a>
+                    </div>
+                </div>
+                <div class="offer-section__cards-card">
+                    <div class="card-icon">
+                        <img src="/icons/030-wrench.svg" alt="wrench" />
+                    </div>
+                    <div class="card-description">
+                        <h3>Prace Konsweratorskie</h3>
+                        <a class="konsweratorskie">więcej</a>
+                    </div>
+                </div>
+                <div class="offer-section__cards-card">
+                    <div class="card-icon">
+                        <img src="/icons/solar-panel.svg" alt="solar-panel" />
+                    </div>
+                    <div class="card-description">
+                        <h3>Fotowoltaika</h3>
+                        <a class="fotowoltaika">więcej</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="offer-section__cards-card-details ogolne">
+                <div class="card-details-header">
+
+                    <a href="#oferta" class="close"></a>
+                    <img src="/icons/023-wrench.svg" alt="wrench" />
+                    <h3>Usługi Ogólnobudowlane</h3>
+                </div>
+                <div class="card-details-description">
+                    <ul>
+                        <li>Roboty ziemne (wykopy, odwierty, drenaże, utwardzanie dróg)</li>
+                        <li>Izolacje fundamentów</li>
+                        <li>Wykończenia tarasów i balkonów</li>
+                        <li>Zewnętrzne elementy dekoracyjne</li>
+                        <li>Aranżacja ogrodu i otoczenia domu (mała arichitektura psoesji prywatynch. Tarasy, chodniki,
+                            schody, wjazdy, alejki i drogi osiedlowe)</li>
+                        <li>Roboty brukarskie</li>
+                        <li>Przygotowanie i wykonanie podjazdów</li>
+                        <li>Ogrodzenia, bramy wjazdowe</li>
+                        <li>Rozbiórka obiektów budowlanych</li>
+                        <li>Ocieplanie dachów</li>
+                        <li>Ocieplenia i elewacje budynków</li>
+                        <li>Deski elewacyjne</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div data-aos="fade-left" class="offer-section__cards-card-details wykonczenia">
+                <div class="card-details-header">
+
+                    <img src="/icons/019-paint roller.svg" alt="roller" />
+                    <h3>Wykończenia</h3>
+                    <a href="#oferta" class="close"></a>
+                </div>
+                <div class="card-details-description">
+                    <ul>
+                        <li>Kompleksowe remonty wnętrz</li>
+                        <li>Układanie płytek / podłóg</li>
+                        <li>Montaż ceramiki / baterii</li>
+                        <li>Montaż oświetlenia</li>
+                        <li>Malowanie i tapetowanie oraz szpachlowanie</li>
+                        <li>Podwieszanie sufitów (dekoracyjne)</li>
+                        <li>Montaż stolarki okiennej, drzwiowej oraz rolet i parapetów</li>
+                        <li>Cekolowanie, suche zabudowy oraz gładzie ze ścianki G/K</li>
+                        <li>Instalacje elektryczne</li>
+                        <li>Instalacje hydrualiczne</li>
+                        <li>Malowanie ręczne oraz ciśnieniowe</li>
+                        <li>Monitoring</li>
+                        <li>Kosztorysowanie, wycena materiałów oraz doradztwo techniczne</li>
+                        <li>Sztukateria domowa</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div data-aos="fade-left" class="offer-section__cards-card-details konsweratorskie">
+                <div class="card-details-header">
+
+                    <img src="/icons/030-wrench.svg" alt="wrench" />
+                    <h3>Prace konserwatorskie</h3>
+                    <a href="#oferta" class="close"></a>
+                </div>
+                <div class="card-details-description">
+                    <ul>
+                        <li>Renowacja obiektów zabytkowych (piaskowanie)</li>
+                        <li>Praca przy elewacjach ceglanych oraz kamiennych</li>
+                        <li>Sztukateria zabytkowa (rekonstrukcja detali sztukatorskich, stiuków)</li>
+                        <li>Malowanie pomieszczeń</li>
+                        <li>Zabytkowe wyprawy tynkarskie</li>
+                        <li>Opinie techniczne, doradztwo materiałowe</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="offer-section__cards-card-details fotowoltaika">
+                <div class="card-details-header">
+
+                    <img src="/icons/solar-panel.svg" alt="solar-panel" />
+                    <h3>Fotowoltaika</h3>
+                    <a href="#oferta" class="close"></a>
+                </div>
+                <div class="card-details-description">
+                    <ul>
+                        <li>Przygotowanie projektu inwestycyjnego</li>
+                        <li>Dostawa sprzętu</li>
+                        <li>Montaż instalacji fotowoltaicznych</li>
+                        <li>Sporządzenie dokumentacji wykonawczej i po wykonawczej</li>
+                        <li>Przyłączenie i odbiór przez operatora sieci elektroenergetycznej</li>
+                        <li>Serwis instalacji fotowoltaicznych</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        <section id="praca" class="work-section">
+            <div data-aos="fade-in" class="work-section__introduction">
+                <h1>Szukamy możliwości współpracy</h1>
+                <p>Względem ciągłego rozwoju firmy poszukujemy wykwalifikowanych praconików z branży ogólnobudowlanej:
+                    Murarzy,
+                    Tynkarzy, Gipsiarzy, Hydraulików, Elektryków oraz pracowników fizycznych z możliwością przyuczenia
+                    do
+                    zawodu. Praca głównie na terenie województwa Warmińsko - Mazurskiego, Pomorskiego oraz
+                    Mazowieckiego.
+
+                    Jeżeli są Państwo zainteresowani skorzystaniem z usług naszej firmy, zapraszamy do kontaku
+                    telefonicznego,
+                    mailowego lub z wykorzystaniem formularza.
+                </p>
+            </div>
+
+            <div data-aos="fade-in" class="form-section__form-container">
+                <form
+                    action="https://script.google.com/macros/s/AKfycbyF8urDU72mWMtR1vplTPb8JX8dbGwymxeF-EXoFc8DJac_ZyY/exec"
+                    class="gform contact-form" id="contactForm" method="POST" data-email="kontakt@mc-trade.pl">
+
+                    <input type="text" id="fname" name="fname" placeholder="Imię" required>
+
+                    <input type="text" id="lname" name="lname" placeholder="Nazwisko" required>
+
+                    <input type="tel" id="phone" name="phone" placeholder="Telefon" required>
+
+                    <input type="email" id="mail" name="mail" placeholder="E-mail" required>
+
+                    <input type="text" id="subject" name="subject" placeholder="Temat">
+
+                    <input type="text" id="message" name="message" placeholder="Wiadomość" required>
+                    <input type="submit" value="Wyślij">
+                    <div style="display:none" class="thankyou_message">
+                        <h2><em>Dziękujemy!</em><br> Twoja wiadomość została wysłana, skontaktujemy się z tobą w
+                            najbliższym
+                            czasie.
+                        </h2>
+                    </div>
+                </form>
+            </div>
+        </section>
+
+        <?php wp_head();?></main>
